b066c63315b7385a9fcb459ed3120dab00bbec06
Alex Apriamashvili <alex.apriamashvili@safetyculture.io>
2025-04-03 09:39:45 +1100

    Fix Swift Project Manifest

--- Package.swift
+++ Package.swift
@@ -2,19 +2,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "flic2lib",
-    platforms: [
-        .macOS(.v11), .iOS(.v12), .tvOS(.v14)
-    ],
-    products: [
-        .library(
-            name: "flic2lib",
-            targets: ["flic2lib"])
-    ],
-    targets: [
-        .binaryTarget(
-            name: "flic2lib",
-            path: "flic2lib.xcframework"
-        )
-    ]
+  name: "Flic2",
+  platforms: [
+    .macOS(.v11),
+    .iOS(.v12),
+    .tvOS(.v14)
+  ],
+
+  products: [
+    .library(name: "Flic2", targets: ["Flic2", "Flic2XCFramework"]),
+  ],
+
+  targets: [
+    .target(name: "Flic2", dependencies: ["Flic2XCFramework"]),
+    .binaryTarget(name: "Flic2XCFramework", path: "flic2lib.xcframework"),
+  ]
 )
