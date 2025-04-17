8d447227866ad4d3a1b56ec1526a5b5621a610cf
Oskar Öberg <oskar@oskob.me>
2025-04-17 09:25:05 +0200

    name

--- Package.swift
+++ Package.swift
@@ -10,11 +10,11 @@ let package = Package(
   ],
 
   products: [
-    .library(name: "Flic2", targets: ["Flic2", "Flic2XCFramework"]),
+    .library(name: "Flic2a", targets: ["Flic2a", "Flic2XCFramework"]),
   ],
 
   targets: [
-    .target(name: "Flic2", dependencies: ["Flic2XCFramework"]),
+    .target(name: "Flic2b", dependencies: ["Flic2XCFramework"]),
     .binaryTarget(name: "Flic2XCFramework", path: "flic2lib.xcframework"),
   ]
 )
